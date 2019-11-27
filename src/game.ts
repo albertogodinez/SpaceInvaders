92d22bc6ab3f6cf53331006928f55cdb1ec70c28
Alberto <alberto.godinez90@gmail.com>
2019-11-26 18:59:11 -0600

    initial commit

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,27 @@
-import 'phaser';
-
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
-
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
-    }
-
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
-    }
-}
+import "phaser";
+import { X_MAX, Y_MAX } from "./global";
+import Level from "./level";
+import MainMenu from "./mainMenu";
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+  type: Phaser.AUTO,
+  backgroundColor: "#60a797",
+  width: X_MAX, // canvas properties
+  height: Y_MAX, // canvas properties,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: false,
+      debug: false
+    }
+  },
+  scene: [MainMenu, Level]
 };
 
-const game = new Phaser.Game(config);
+export class Game extends Phaser.Game {
+  constructor(config) {
+    super(config);
+  }
+}
+
+const game = new Game(config);
